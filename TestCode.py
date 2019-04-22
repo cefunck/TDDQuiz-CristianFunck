d08937a4f7fc24c22bf1b82c89ddffa53b4d4893
cristian <cefunck@miuandes.cl>
2019-04-22 07:04:48 -0400

    Refactor: improve the name of the first test

--- TestCode.py
+++ TestCode.py
@@ -1,9 +1,10 @@
 import unittest
+from ProductionCode import *
 
 class AddFunctionTestCase(unittest.TestCase):
     def setUp(self):
         self.list_of_numbers = "1,2,3,1"
 
-    def testAddReturnIntValue(self):
+    def testReturnIntValue(self):
         self.assertIsInstance(Add(self.list_of_numbers),int,
                               "Return Error: The result is not int value")
