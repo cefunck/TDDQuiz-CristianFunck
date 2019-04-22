fbaf71a50c9cf2d9eed904920a1f0c2e0bc3b1b9
cristian <cefunck@miuandes.cl>
2019-04-22 08:19:54 -0400

    Added test to validate the use of a optionally delimiter (will fail)

--- TestCode.py
+++ TestCode.py
@@ -5,6 +5,7 @@ class AddFunctionTestCase(unittest.TestCase):
     def setUp(self):
         self.list_of_numbers = "1,2,3,1"
         self.invalid_input = [1,2,3,1]
+        self.input_with_other_delimiter = ";1;2;3;1"
 
     def testReturnIntValue(self):
         self.assertIsInstance(Add(self.list_of_numbers),int,
@@ -17,3 +18,7 @@ class AddFunctionTestCase(unittest.TestCase):
     def testValidateCorrectSum(self):
         self.assertEqual(Add(self.list_of_numbers),7,
                          "Sum Error: The result of sum is incorrect")
+
+    def testOptionallyDelimiter(self):
+        self.assertEqual(Add(self.input_with_other_delimiter,";"), 7,
+                         "Error: Delimiter is not yet implemented")
