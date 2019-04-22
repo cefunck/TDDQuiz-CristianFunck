b5c7ccc8cc8bcaf394c5a0a04a8790c91470ac1e
cristian <cefunck@miuandes.cl>
2019-04-22 08:27:31 -0400

    Green: Added code to dismiss negative numbers (will success)

--- ProductionCode.py
+++ ProductionCode.py
@@ -3,7 +3,8 @@ def Add(string_numbers, delimiter = None):
     if not delimiter: delimiter = ","
     try:
         for number in string_numbers.split(delimiter):
-            if number != "": result += int(number)
+            if number != "" and "-" not in number:
+                result += int(number)
     except TypeError:
         print("Input must be a string of numbers delimited by commas.")
     return result
