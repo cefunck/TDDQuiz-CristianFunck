44a05c39ea4cb7e95de0a1a3426dc834ed37cd68
cristian <cefunck@miuandes.cl>
2019-04-22 08:20:51 -0400

    Added code to support a optionally delimiter (will success)

--- ProductionCode.py
+++ ProductionCode.py
@@ -1,8 +1,9 @@
-def Add(string_numbers):
+def Add(string_numbers, delimiter = None):
     result = 0
+    if not delimiter: delimiter = ","
     try:
-        for number in string_numbers.split(","):
-            result += int(number)
+        for number in string_numbers.split(delimiter):
+            if number != "": result += int(number)
     except TypeError:
         print("Input must be a string of numbers delimited by commas.")
     return result
