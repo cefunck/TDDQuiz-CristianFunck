b94744618936c5f0737ad63fd22ccd71db8dbc9f
cristian <cefunck@miuandes.cl>
2019-04-22 08:09:03 -0400

    Green: Added code to sum correctly (will success)

--- ProductionCode.py
+++ ProductionCode.py
@@ -1,7 +1,8 @@
 def Add(string_numbers):
     result = 0
     try:
-        string_numbers.split(",")
+        for number in string_numbers.split(","):
+            result += int(number)
     except TypeError:
         print("Input must be a string of numbers delimited by commas.")
     return result
