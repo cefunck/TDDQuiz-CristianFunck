cdca94f0e36d9d4d8aa1663698aeeeb6377fee3d
cristian <cefunck@miuandes.cl>
2019-04-22 07:45:08 -0400

    Red: Added a test to validate a input error throw (will fail)

--- ProductionCode.py
+++ ProductionCode.py
@@ -1,3 +1,4 @@
 def Add(string_numbers):
     result = 0
+    string_numbers.split(",")
     return result
--- TestCode.py
+++ TestCode.py
@@ -4,7 +4,12 @@ from ProductionCode import *
 class AddFunctionTestCase(unittest.TestCase):
     def setUp(self):
         self.list_of_numbers = "1,2,3,1"
+        self.invalid_input = [1,2,3,1]
 
     def testReturnIntValue(self):
         self.assertIsInstance(Add(self.list_of_numbers),int,
                               "Return Error: The result is not int value")
+
+    def testValidateInput(self):
+        with self.assertRaises(TypeError):
+            Add(self.invalid_input)
